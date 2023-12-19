05d96bd8a3abbc79d5457c0efeb67acbfa6d7862
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-12-19 17:02:43 +0100

    Add Day03 part 2 puzzle solution code and output

--- Day03/EngineSchematicAnalyzer.cs
+++ Day03/EngineSchematicAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Dynamic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using Position = (int r, int c);
 
 namespace Day03;
@@ -20,6 +21,33 @@ public class EngineSchematicAnalyzer
         return numbers.Sum();
     }
 
+    public int GetSumOfGearRatios()
+    {
+        var ratios = GetGearRatios();
+        return ratios.Sum();
+    }
+
+    private List<int> GetGearRatios()
+    {
+        List<int> output = new();
+        List<Position> gearPositions = GetGearPositions();
+        List<(Position?, Position?)> digitPositions = GetDigitsPositions();
+
+        foreach (var position in gearPositions)
+        {
+            var adjacentRanges = digitPositions
+                .Where(r => IsAdjacentToRange(position, r))
+                .ToList();
+
+            if (adjacentRanges.Count != 2)
+                continue;
+
+            output.Add(ParseDigits(adjacentRanges[0]) * ParseDigits(adjacentRanges[1]));
+        }
+
+        return output;
+    }
+
     private List<int> GetPartNumbers()
     {
         List<int> output = new();
@@ -37,17 +65,13 @@ public class EngineSchematicAnalyzer
 
     private bool VerifyPartNumber((Position? start, Position? end) position, out int number)
     {
-        if (position.start is null || position.end is null)
-            throw new ArgumentNullException("position");
+        number = ParseDigits(position);
+
+        bool IsPartNumber = false;
 
         int row = (int)position.start?.r!;
         int start = (int)position.start?.c!;
         int end = (int)position.end?.c!;
-        int.TryParse(inputLines[row].Substring(start, end - start + 1),
-                     out number);
-
-        bool IsPartNumber = false;
-
         Position surroundStart = (Math.Max(row - 1, 0), Math.Max(start - 1, 0));
         Position surroundEnd = (Math.Min(row + 1, inputLines.Length -1), end + 1);
 
@@ -65,6 +89,22 @@ public class EngineSchematicAnalyzer
         }
 
         return IsPartNumber;
+    }
+
+    private int ParseDigits((Position? start, Position? end) position)
+    {
+        int number = 0;
+
+        if (position.start is null || position.end is null)
+            throw new ArgumentNullException("position");
+
+        int row = (int)position.start?.r!;
+        int start = (int)position.start?.c!;
+        int end = (int)position.end?.c!;
+        int.TryParse(inputLines[row].Substring(start, end - start + 1),
+                     out number);
+
+        return number;
     }
 
     private List<(Position? start, Position? end)> GetDigitsPositions()
@@ -110,5 +150,37 @@ public class EngineSchematicAnalyzer
         }
 
         return output;
+    }
+
+    private List<Position> GetGearPositions()
+    {
+        List<Position> output = new();
+
+        for (int i = 0; i < inputLines.Length; i++)
+        {
+            for (int j = 0; j < inputLines[i].Length; j++)
+            {
+                if (inputLines[i][j] == '*')
+                {
+                    output.Add((i, j));
+                }
+            }
+        }
+
+        return output;
+    }
+
+    private bool IsAdjacentToRange(Position position, (Position? start, Position? end) range)
+    {
+        Position surroundStart = (position.r - 1, position.c - 1);
+        Position surroundEnd = (position.r + 1, position.c + 1);
+
+        if (range.start?.r > surroundEnd.r
+            || range.start?.c > surroundEnd.c
+            || range.end?.r < surroundStart.r
+            || range.end?.c < surroundStart.c)
+            return false;
+        else
+            return true;
     }
 }
--- Day03/Program.cs
+++ Day03/Program.cs
@@ -5,3 +5,7 @@ EngineSchematicAnalyzer analyzer = new("input.txt");
 int sumOfPartNumbers = analyzer.GetSumOfPartNumbers();
 
 Console.WriteLine($"Part 1: {sumOfPartNumbers}");
+
+int sumOfGearRatios =  analyzer.GetSumOfGearRatios();
+
+Console.WriteLine($"Part 2: {sumOfGearRatios}");
