febb8632ac8c901d32b4d24e09bf5c3271a7c169
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-06 11:15:26 +0100

    Add Day10 part 2 puzzle solution code and output

--- Day10/PipeNavigator.cs
+++ Day10/PipeNavigator.cs
@@ -2,12 +2,14 @@
 public class PipeNavigator
 {
     List<Tile> tiles = new();
+    char[][] inputChars;
+    char startSymbol;
     public PipeNavigator(string filePath)
     {
         if (File.Exists(filePath) == false)
             throw new FileNotFoundException($"File {filePath} not found.");
 
-        var inputChars = File.ReadAllLines(filePath)
+        inputChars = File.ReadAllLines(filePath)
             .Select(l => l.ToCharArray())
             .ToArray();
 
@@ -30,6 +32,8 @@ public class PipeNavigator
         {
             start.Connections.Add((tile.X, tile.Y));
         }
+
+        startSymbol = DetermineSymbolUsingConnections(start);
     }
 
     public int CountStepsToFarthest()
@@ -85,7 +89,84 @@ public class PipeNavigator
         return loop.Select(s => s.steps).Max();
     }
 
-    public List<Tile> FindConnected(Tile tile)
+    public int CountTilesWithinLoop()
+    {
+        int tileCount = 0;
+        List<Tile> loop = FindLoop();
+
+        for (int y = 0; y < inputChars.Length; y++)
+        {
+            bool insideLoop = false;
+            char lastBend = ' ';
+            for (int x = 0; x < inputChars[y].Length; x++)
+            {
+                if (loop.Any(t => t.X == x && t.Y == y))
+                {
+                    char symbol = inputChars[y][x];
+                    if (symbol == 'S')
+                        symbol = startSymbol;
+
+                    switch (symbol)
+                    {
+                        case '|':
+                            insideLoop = !insideLoop;
+                            break;
+                        case 'L':
+                            lastBend = 'L';
+                            break;
+                        case 'F':
+                            lastBend = 'F';
+                            break;
+                        case 'J':
+                            if (lastBend == 'F')
+                                insideLoop = !insideLoop;
+                            break;
+                        case '7':
+                            if (lastBend == 'L')
+                                insideLoop = !insideLoop;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (insideLoop)
+                    tileCount++;
+            }
+        }
+
+        return tileCount;
+    }
+
+    private List<Tile> FindLoop()
+    {
+        List<Tile> loop = new();
+        Tile start = tiles
+            .Where(t => t.Symbol == 'S')
+            .First();
+        loop.Add(start);
+        Tile previous = start;
+        Tile current = FindConnected(previous).First();
+        loop.Add(current);
+
+        bool loopClosed = false;
+        while (loopClosed == false)
+        {
+            Tile next = FindConnected(current)
+                .Where(t => t != previous)
+                .First();
+
+            if (next.Symbol == 'S')
+                loopClosed = true;
+            else
+            {
+                loop.Add(next);
+                previous = current;
+                current = next;
+            }
+        }
+        return loop;
+    }
+    private List<Tile> FindConnected(Tile tile)
     {
         List<Tile> output = new List<Tile>();
 
@@ -99,5 +180,41 @@ public class PipeNavigator
                 output.Add(connectedTile);
         }
         return output;
+    }
+
+    private char DetermineSymbolUsingConnections(Tile tile)
+    {
+        if (tile.Connections.Count == 0)
+        {
+            return '.';
+        }
+        else if (tile.Connections.Count == 2)
+        {
+            List<(int x, int y)> differences = new();
+            foreach (var connection in tile.Connections)
+            {
+                differences.Add((connection.x - tile.X, connection.y - tile.Y));
+            }
+
+            if (differences.Contains((0, -1))
+                && differences.Contains((0, 1)))
+                return '|';
+            if (differences.Contains((-1, 0))
+                && differences.Contains((1, 0)))
+                return '-';
+            if (differences.Contains((0, -1))
+                && differences.Contains((1, 0)))
+                return 'L';
+            if (differences.Contains((0, -1))
+                && differences.Contains((-1, 0)))
+                return 'J';
+            if (differences.Contains((0, 1))
+                && differences.Contains((-1, 0)))
+                return '7';
+            if (differences.Contains((0, 1))
+                && differences.Contains((1, 0)))
+                return 'F';
+        }
+        return tile.Symbol;
     }
 }
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -5,3 +5,7 @@ PipeNavigator navigator = new("input.txt");
 int steps = navigator.CountStepsToFarthest();
 
 Console.WriteLine($"Part 1: {steps}");
+
+int tiles = navigator.CountTilesWithinLoop();
+
+Console.WriteLine($"Part 2: {tiles}");
